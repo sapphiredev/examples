00c817511fc9642bb0caf23fd6fdde2590974be9
Jeroen Claassens <support@favware.tech>
2021-02-22 12:55:42 +0100

    chore: my boss forced me to build this feature... pure shit.

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,4 +1,5 @@
 import { ApplyOptions } from '@sapphire/decorators';
+import { MessagePrompter } from '@sapphire/discord.js-utilities';
 import { Command, CommandOptions } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
@@ -9,6 +10,10 @@ import type { Message } from 'discord.js';
 export class UserCommand extends Command {
 	public async run(message: Message) {
 		const msg = await message.channel.send('Ping?');
+		const handler = new MessagePrompter('Are you sure you want to continue?');
+
+		const result = await handler.run(message.channel, message.author);
+		console.log(result);
 		return msg.edit(
 			`Pong! Bot Latency ${Math.round(this.context.client.ws.ping)}ms. API Latency ${msg.createdTimestamp - message.createdTimestamp}ms.`
 		);
