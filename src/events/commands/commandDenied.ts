f5be91e168cdb28050ee85f9fd6d763469551cf4
Jeroen Claassens <support@favware.tech>
2021-07-15 01:24:09 +0200

    refactor: better commandDenied

--- src/events/commands/commandDenied.ts
+++ src/events/commands/commandDenied.ts
@@ -1,8 +1,10 @@
-import type { CommandDeniedPayload } from '@sapphire/framework';
+import type { CommandDeniedPayload, Events } from '@sapphire/framework';
 import { Event, UserError } from '@sapphire/framework';
 
-export class UserEvent extends Event<'commandDenied'> {
+export class UserEvent extends Event<Events.CommandDenied> {
 	public async run({ context, message: content }: UserError, { message }: CommandDeniedPayload) {
+		// `context: { silent: true }` should make UserError silent:
+		// Use cases for this are for example permissions error when running the `eval` command.
 		if (Reflect.get(Object(context), 'silent')) return;
 
 		return message.channel.send(content, { allowedMentions: { users: [message.author.id], roles: [] } });
