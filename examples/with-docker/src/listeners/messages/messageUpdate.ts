2c47400ce17fcf71ff3188dae976b1cce91466fc
Jurien Hamaker <whoami@jurien.dev>
2021-08-12 12:38:00 +0200

    feat: docker example

---
+++ examples/with-docker/src/listeners/messages/messageUpdate.ts
@@ -0,0 +1,21 @@
+import { Listener } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+export class UserEvent extends Listener {
+	public run(old: Message, message: Message) {
+		// If the contents of both messages are the same, return:
+		if (old.content === message.content) return;
+
+		// If the message was sent by a webhook, return:
+		if (message.webhookId !== null) return;
+
+		// If the message was sent by the system, return:
+		if (message.system) return;
+
+		// If the message was sent by a bot, return:
+		if (message.author.bot) return;
+
+		// Run the message parser.
+		this.container.client.emit('preMessageParsed', message);
+	}
+}
