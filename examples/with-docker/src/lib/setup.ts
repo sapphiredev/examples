2b258a2122112925f00d14d6b0b7114190343817
Jurien Hamaker <whoami@jurien.dev>
2021-08-12 23:41:00 +0200

    feat: add with-docker example (#4)
    Co-authored-by: Jeroen Claassens <support@favware.tech>

---
+++ examples/with-docker/src/lib/setup.ts
@@ -0,0 +1,11 @@
+import '@sapphire/plugin-logger/register';
+import '@sapphire/plugin-api/register';
+import '@skyra/editable-commands';
+import { options as coloretteOptions } from 'colorette';
+import { inspect } from 'util';
+
+// Set default inspection depth
+inspect.defaultOptions.depth = 1;
+
+// Enable colorette
+coloretteOptions.enabled = true;
