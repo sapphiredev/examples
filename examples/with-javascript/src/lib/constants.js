080c6535c532bbd226a52e40f9c6aa1cc7a5247b
Jeroen Claassens <support@favware.tech>
2021-08-12 21:42:22 +0200

    chore: duh
    permalink: http://whatthecommit.com/56d6c4f53110a7162751d9e91d7f0859

--- examples/with-javascript/src/lib/constants.js
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-const rootDir = join(__dirname, '..', '..');
-const srcDir = join(rootDir, 'src');
-
-module.exports = {
-	rootDir,
-	srcDir
-};
--- examples/with-javascript/src/sample-bot.js
+++ examples/with-javascript/src/sample-bot.js
@@ -1,9 +1,10 @@
 require('./lib/setup');
 const { LogLevel, SapphireClient } = require('@sapphire/framework');
+const { prefix, discord_token } = require('./config.json')
 
 const client = new SapphireClient({
-	defaultPrefix: process.env.DEFAULT_PREFIX,
-	regexPrefix: /^(hey +)?dragon[,! ]/i,
+	defaultPrefix: prefix,
+	regexPrefix: /^(hey +)?bot[,! ]/i,
 	caseInsensitiveCommands: true,
 	logger: {
 		level: LogLevel.Debug
@@ -25,7 +26,7 @@ const client = new SapphireClient({
 const main = async () => {
 	try {
 		client.logger.info('Logging in');
-		await client.login();
+		await client.login(discord_token);
 		client.logger.info('logged in');
 	} catch (error) {
 		client.logger.fatal(error);
