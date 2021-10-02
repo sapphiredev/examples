3e27125d60e005d4da1175f0f467650298741883
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-10-02 21:12:43 +0530

    feat: update for new colorette version (#21)
    closes #22

--- examples/with-javascript/src/lib/setup.js
+++ examples/with-javascript/src/lib/setup.js
@@ -1,11 +1,11 @@
 require('@sapphire/plugin-logger/register');
 require('@sapphire/plugin-api/register');
 require('@sapphire/plugin-editable-commands/register');
-const { options: coloretteOptions } = require('colorette');
+const { createColors } = require('colorette');
 const { inspect } = require('util');
 
 // Set default inspection depth
 inspect.defaultOptions.depth = 1;
 
 // Enable colorette
-coloretteOptions.enabled = true;
+createColors({ useColor: true });
