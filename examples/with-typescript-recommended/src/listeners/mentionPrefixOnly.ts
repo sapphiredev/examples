127aa95af82ee4df1662bb13d740481e04e2caaa
Zoriot <59896907+Zoriot@users.noreply.github.com>
2022-08-19 14:12:05 +0200

    Update examples/with-typescript-recommended/src/listeners/mentionPrefixOnly.ts
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- examples/with-typescript-recommended/src/listeners/mentionPrefixOnly.ts
+++ examples/with-typescript-recommended/src/listeners/mentionPrefixOnly.ts
@@ -5,6 +5,6 @@ import type { Message } from 'discord.js';
 export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
 	public async run(message: Message) {
 		const prefix = this.container.client.options.defaultPrefix;
-		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any Prefix for Chat-Commands.');
+		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any Prefix for Message Commands.');
 	}
 }
