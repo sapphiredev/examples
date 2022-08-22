8555c4c2124bb08c597aec041581f44b2c8e3e55
Tommaso Chiti <chitilivorno@gmail.com>
2022-08-22 10:49:15 +0200

    fix(with-docker): added missing dotenv-cra dev-dependency (#97)
    Co-authored-by: Jeroen Claassens <jeroen.claassens@live.nl>
    Co-authored-by: Jeroen Claassens <j.claassens@cgi.com>

--- examples/with-docker/src/lib/setup.ts
+++ examples/with-docker/src/lib/setup.ts
@@ -1,8 +1,17 @@
+// Unless explicitly defined, set NODE_ENV as development:
+process.env.NODE_ENV ??= 'development';
+
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-editable-commands/register';
 import '@sapphire/plugin-logger/register';
 import * as colorette from 'colorette';
+import { config } from 'dotenv-cra';
+import { join } from 'path';
 import { inspect } from 'util';
+import { srcDir } from './constants';
+
+// Read env var
+config({ path: join(srcDir, '.env') });
 
 // Set default inspection depth
 inspect.defaultOptions.depth = 1;
