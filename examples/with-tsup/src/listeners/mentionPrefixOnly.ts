7de8f13cd2183d6bf84134bb4dd3731bcf6e5828
Seren_Modz 21 <seren@kings-world.net>
2024-09-12 12:03:56 +0100

    fix: make use of BaseChannel#isSendable()
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- examples/with-tsup/src/listeners/mentionPrefixOnly.ts
+++ examples/with-tsup/src/listeners/mentionPrefixOnly.ts
@@ -1,12 +1,11 @@
 import type { Events } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
-import { ChannelType, type Message } from 'discord.js';
+import type { Message } from 'discord.js';
 
 export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
-	public override async run(message: Message) {
-		// TODO: replace with BaseChannel#isSendable once discord.js releases a new version
-		// Bots cannot send messages in group DMs, so we have no choice but to return.
-		if (message.channel.type === ChannelType.GroupDM) return;
+	public override run(message: Message) {
+		// Do nothing if we cannot send messages in the channel (eg. group DMs)
+		if (!message.channel.isSendable()) return;
 
 		const prefix = this.container.client.options.defaultPrefix;
 		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any Prefix for Message Commands.');
