4a85a887a2e4ac589667195088ea0e23d3471caa
Seren_Modz 21 <seren@kings-world.net>
2024-11-02 19:52:03 +0000

    feat: add support for user-installable apps (#228)
    * chore(deps): update discord.js and sapphire related dependencies
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * feat: add support for user-installable apps
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * fix: a temporary solution to the type error
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * refactor: remove old todo and add todo's for other channel type checks
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * fix: make use of BaseChannel#isSendable()
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * chore(deps): update discord.js and sapphire dependencies
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * chore(deps): update sapphire decorators and framework
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    
    * chore(deps): update editable commands plugin
    
    * chore: update deps
    
    ---------
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- examples/with-typescript-starter/src/listeners/mentionPrefixOnly.ts
+++ examples/with-typescript-starter/src/listeners/mentionPrefixOnly.ts
@@ -3,7 +3,10 @@ import { Listener } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
 export class UserEvent extends Listener<typeof Events.MentionPrefixOnly> {
-	public override async run(message: Message) {
+	public override run(message: Message) {
+		// Do nothing if we cannot send messages in the channel (eg. group DMs)
+		if (!message.channel.isSendable()) return;
+
 		const prefix = this.container.client.options.defaultPrefix;
 		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any Prefix for Message Commands.');
 	}
